fdb500bdbde7a09b4ebf1a1921e04571ad5a6df5
Elianedantas <edantas241@gmail.com>
2020-05-29 17:16:59 -0300

    funçao01.js

---
+++ Aula16/funçao01.js
@@ -0,0 +1,15 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+/*
+let res = parimpar(4)
+console.log(res)
+OU
+*/
+
+console.log(parimpar(4))
