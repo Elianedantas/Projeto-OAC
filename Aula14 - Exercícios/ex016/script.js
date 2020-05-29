a1f1ca0881d0d53a7002f1d89104b26d913c351a
Elianedantas <edantas241@gmail.com>
2020-05-29 12:42:55 -0300

    ex016

--- Aula14 - Exercícios/ex016/script.js
+++ Aula14 - Exercícios/ex016/script.js
@@ -1,5 +1,30 @@
 function contar() {
-    var inicio
-    var fim
-    var passo
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        // length -> diz o tamanho da var..
+        res.innerHTML = `Impossível contar!`
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1.')
+            p = 1
+        }
+        if (i < f) { // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+                //https://unicode.org/emoji/charts/full-emoji-list.html#1f603
+            }
+        } else { // Contagem decrescente
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
