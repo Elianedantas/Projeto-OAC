eeffcf698c27177ed9f3643f5a59794810259b93
Elianedantas <edantas241@gmail.com>
2020-05-30 07:06:43 -0300

    d001

---
+++ Desafios/d001/desafio-mensagens/script.js
@@ -0,0 +1,9 @@
+function clicar1() {
+    alert('Você clicou no 1° botão!')
+}
+function clicar2() {
+    alert('Você clicou no 2° botão!')
+}
+function clicar3() {
+    alert('Você clicou no 3° botão!')
+}
