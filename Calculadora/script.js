691ed7f9fb17a9be71ce1bcd7ffeb98f5f0bca3c
Elianedantas <edantas241@gmail.com>
2020-06-03 20:30:05 -0300

    calculadora
    mais operaçoes

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,17 +1,45 @@
 var resultado = 0
+var numero = ''
+var termo = [undefined, undefined, undefined]
 function preencher(num) {
     document.getElementById('visor').value += num
+    numero += num
 }
 function limpar() {
     document.getElementById('visor').value = ''
+    numero = ''
+}
+function operador(op) {
+    termo[0] = numero
+    termo[1] = op
+    document.getElementById('visor').value += op
+    numero = ''
 }
 function calcular() {
-    resultado = document.getElementById('visor').value
-    document.getElementById('visor').value = ''
-    document.getElementById('visor').value = eval(resultado)
-}
-function raiz(simb) {
-    document.getElementById('visor').value += simb
-    resultado += Math.sqrt()
-    document.getElementById('visor').value = eval(resultado)
+    termo[2] = numero
+    switch (termo[1]) {
+        case '+':
+            resultado = Number(termo[0]) + Number(termo[2])
+            break
+        case '-':
+            resultado = Number(termo[0]) - Number(termo[2])
+            break
+        case '*':
+            resultado = Number(termo[0]) * Number(termo[2])
+            break
+        case '/':
+            resultado = Number(termo[0]) / Number(termo[2])
+            break
+        case '**':
+            resultado = Number(termo[0]) ** Number(termo[2])
+            break
+        case '%':
+            resultado = Number(termo[0]) / 100
+            break
+        case 'raiz':
+            alert(resultado)
+            resultado = Number(termo[0]) ** 0.5
+            break
+    }
+    document.getElementById('visor').value = resultado
 }
