92af9012a431b59e1fb72aaf1c78d4bc27d3404d
Elianedantas <edantas241@gmail.com>
2020-06-03 14:32:06 -0300

    calculadora (atualização)

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,35 +1,17 @@
-var oper = ''
-var gvisor = ''
-var gvalor = ''
-var goper = ''
-var valor1 = ''
-var valor2 = ''
-function calcnum(num) {
-    if (typeof gvisor == 'undefined') {
-        document.getElementById('visor').value = '';
-    }
-    document.getElementById('visor').value = document.getElementById('visor').value + num;
-    gvisor = 1;
+var resultado = 0
+function preencher(num) {
+    document.getElementById('visor').value += num
 }
-function calcLimpar() {
-    document.getElementById('visor').value = '';
-    delete gvalor;
-    delete goper;
-    delete gvisor;
+function limpar() {
+    document.getElementById('visor').value = ''
 }
-function calcOper(oper, valor1, valor2) {
-    if (oper == "somar") {
-        var valor = parseFloat(valor1) + parseFloat(valor2);
-    } else {
-        if (oper == "subtrair") {
-            var valor = valor1 - valor2;
-        } else {
-            if (oper == "multiplicar") {
-                var valor = valor1 * valor2;
-            } else {
-                var valor = valor1 / valor2;
-            }
-        }
-    }
-    return(valor);
+function calcular() {
+    resultado = document.getElementById('visor').value
+    document.getElementById('visor').value = ''
+    document.getElementById('visor').value = eval(resultado)
+}
+function raiz(simb) {
+    document.getElementById('visor').value += simb
+    resultado += Math.sqrt()
+    document.getElementById('visor').value = eval(resultado)
 }
