9601dbbdb614cde395500001ca368fcfa7aed404
Eliane Dantas <edantas241@gmail.com>
2022-11-25 11:16:54 -0300

    Delete desafios

--- Desafios/d001/desafio-mensagens/script.js
+++
@@ -1,9 +0,0 @@
-function clicar1() {
-    alert('Você clicou no 1° botão!')
-}
-function clicar2() {
-    alert('Você clicou no 2° botão!')
-}
-function clicar3() {
-    alert('Você clicou no 3° botão!')
-}
